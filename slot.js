5cf8ec7db5e293e3ac2633d4e50abf36fd89b7b0
stuartdavidmitchell <64138582+stuartdavidmitchell@users.noreply.github.com>
2020-07-14 11:58:44 +0100

    Add files via upload

--- slot.js
+++ slot.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     * Global variables
     */
     var completed = 0,
-        imgHeight = 4800,
+        imgHeight = 4500,
         posArr = [
             75, //image1
             375, //image2 
@@ -34,18 +34,17 @@ $(document).ready(function() {
             4275, //bar
             4575, //guava
             4875, //banana
-            5175 //cherry
         ];
-    
-
+    var flood = 0;
+    var r4stopping= 0;
     
     var win = [];
-    win[75]   = win[1875] = win[3675] = 1;
-    win[375]  = win[2175] = win[3975] = 2;
-    win[675]  = win[2475] = win[4275] = 3;
-    win[975]  = win[2775] = win[4575] = 4;
-    win[1275] = win[3075] = win[4875] = 5;
-    win[1575] = win[3375] = win[5175] = 6;
+    win[75]   = win[1875] = win[3675] = 3;
+    win[375]  = win[2175] = win[3975] = 1;
+    win[675]  = win[2475] = win[4275] = 2;
+    win[975]  = win[2775] = win[4575] = 3;
+    win[1275] = win[3075] = win[4875] = 1;
+    win[1575] = win[3375] = win[5175] = 2;
     
     /**
     * @class Slot
@@ -127,6 +126,17 @@ $(document).ready(function() {
         pos = pos.split(' ')[1];
         pos = parseInt(pos, 10);
 
+        /*
+         * If r4pos is >0 then that indicates that the stopping reel is the result reel, and the final
+         * position will depend on the flood/no flood outcome.
+         *
+         */
+        if (r4stopping > 0) {
+            if (flood) { pos = 500;
+            } else {     pos = 800;
+            }
+            r4stopping = 0;
+        }
         for(i = 0; i < posArr.length; i++) {
             for(j = 0;;j++) {
                 k = posArr[i] + (imgHeight * j);
@@ -153,15 +163,6 @@ $(document).ready(function() {
         });
     }; 
     
-    var myVideo = document.getElementById("video1"); 
-    function playPause() { 
-        alert("in playPause");
-        myVideo.src('clipxx.mp4');
-        if (myVideo.paused) 
-            myVideo.play(); 
-        else 
-            myVideo.pause(); 
-    } 
 
 
     /**
@@ -171,7 +172,7 @@ $(document).ready(function() {
     Slot.prototype.reset = function() {
         var el_id = $(this.el).attr('id');
         $._spritely.instances[el_id].t = 0;
-        $(this.el).css('background-position', '0px 75px');
+        $(this.el).css('background-position', '0px 375px');
         this.speed = 0;
         completed = 0;
         $('#result').html('');
@@ -193,15 +194,97 @@ $(document).ready(function() {
         $('#check').attr("disabled", true);
     }
     
+    var newSrc;
+    var videofiles = [];
+    videofiles[333] = 'videos/01-nofloodnorain.mp4';
+    videofiles[332] = 'videos/01-nofloodnorain.mp4';
+    videofiles[331] = 'videos/01-nofloodnorain.mp4';
+    videofiles[323] = 'videos/02-nofloodwithrain.mp4';
+    videofiles[322] = 'videos/02-nofloodwithrain.mp4';
+    videofiles[321] = 'videos/03-floodsondry.mp4';
+    videofiles[313] = 'videos/03-floodsondry.mp4';
+    videofiles[312] = 'videos/03-floodsondry.mp4';
+    videofiles[311] = 'videos/03-floodsondry.mp4';
+    videofiles[233] = 'videos/01-nofloodnorain.mp4';
+    videofiles[232] = 'videos/01-nofloodnorain.mp4';
+    videofiles[231] = 'videos/01-nofloodnorain.mp4';
+    videofiles[223] = 'videos/02-nofloodwithrain.mp4';
+    videofiles[222] = 'videos/02-nofloodwithrain.mp4';
+    videofiles[221] = 'videos/02-nofloodwithrain.mp4';
+    videofiles[213] = 'videos/02-nofloodwithrain.mp4';
+    videofiles[212] = 'videos/04-floodheavyrain.mp4';
+    videofiles[211] = 'videos/04-floodheavyrain.mp4';
+    videofiles[133] = 'videos/01-nofloodnorain.mp4';
+    videofiles[132] = 'videos/01-nofloodnorain.mp4';
+    videofiles[131] = 'videos/01-nofloodnorain.mp4';
+    videofiles[123] = 'videos/06-floods.mp4';
+    videofiles[122] = 'videos/06-floods.mp4';
+    videofiles[121] = 'videos/05-sevfloods.mp4';
+    videofiles[113] = 'videos/06-floods.mp4';
+    videofiles[112] = 'videos/05-sevfloods.mp4';
+    videofiles[111] = 'videos/05-sevfloods.mp4';
+
+    var outcomes = [];
+    outcomes[333] = 0;
+    outcomes[332] = 0;
+    outcomes[331] = 0;
+    outcomes[323] = 0;
+    outcomes[322] = 0;
+    outcomes[321] = 1;
+    outcomes[313] = 1;
+    outcomes[312] = 1;
+    outcomes[311] = 2;
+    outcomes[233] = 0;
+    outcomes[232] = 0;
+    outcomes[231] = 0;
+    outcomes[223] = 0;
+    outcomes[222] = 0;
+    outcomes[221] = 1;
+    outcomes[213] = 1;
+    outcomes[212] = 2;
+    outcomes[211] = 2;
+    outcomes[133] = 0;
+    outcomes[132] = 0;
+    outcomes[131] = 0;
+    outcomes[123] = 1;
+    outcomes[122] = 1;
+    outcomes[121] = 2;
+    outcomes[113] = 2;
+    outcomes[112] = 2;
+    outcomes[111] = 2;
+
+
     function printResult() {
-        var res;
-        if(win[a.pos] === win[b.pos] && win[a.pos] === win[c.pos]) {
-            res = "";
-        } else {
-            res = "";
-        }
-        $('#result').html(res);
+        var res1, res2;
+        var s1index = win[a.pos];
+        var s2index = win[b.pos];
+        var s3index = win[c.pos];
+        res1 = a.pos.toString()+' '+b.pos.toString()+' '+c.pos.toString();
+        res2 = s1index.toString() + s2index.toString() + s3index.toString(); 
+        newSrc=videofiles[parseInt(res2)];
+        if (outcomes[parseInt(res2)] == 0) 
+        { flood=0; }
+        else if (outcomes[parseInt(res2)] == 1) 
+        { flood = Math.round(Math.random()); }
+        else 
+        { flood = 1; }
+          
+	$('#result').html(res1+'\n'+res2+'\n'+newSrc+flood.toString());
+        $('#videofile').html(newSrc);
     }
+
+    var myVideo = document.getElementById("video1"); 
+    var source = document.createElement('source');
+    var ftt = 1;
+
+    function playPause() { 
+        if (ftt) { ftt=0; }
+        else { myVideo.removeChild(source); }
+        source.setAttribute('src',newSrc);
+        myVideo.appendChild(source);
+        myVideo.load();
+        myVideo.play();
+    } 
 
     //create slot objects
     var a = new Slot('#slot1', 30, 1),
@@ -233,7 +316,7 @@ $(document).ready(function() {
             a.stop();
             b.stop();
             c.stop();
-            this.innerHTML = "Reset";
+            this.innerHTML = "Start";
 
             disableControl(); //disable control until the slots stop
 
@@ -262,7 +345,6 @@ $(document).ready(function() {
     */
     $('#check').click(function() {
         var x;
-        var myVideo = document.getElementById("playvid");
 
         if(this.innerHTML == "Start") {
             d.start();
@@ -279,9 +361,10 @@ $(document).ready(function() {
                 }
             }, 100);
         } else if(this.innerHTML == "Stop") {
+            r4stopping = 1;
             d.stop();
-            this.innerHTML = "Reset";
-            playPause();
+            this.innerHTML = "Start";
+
             disableCheck(); //disable control until the slots stop
 
             //check every 100ms if slots have stopped
@@ -290,10 +373,10 @@ $(document).ready(function() {
                 if(d.speed === 0) {
                     enableCheck();
                     window.clearInterval(x);
+                    playPause();
                     printResult();
                 }
             }, 100);
-
         } else { //reset
             d.reset();
             this.innerHTML = "Start";
